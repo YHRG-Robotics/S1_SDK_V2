a1e8092813cf68a062ed49ddc89ede784e835028
sugerbyu <1281262604@qq.com>
2026-03-18 13:40:23 +0800

    精简依赖，调整编译脚本

--- build.sh
+++ build.sh
@@ -1,24 +1,32 @@
 #!/bin/bash
+set -e
+
 get_arch=$(uname -m)
-echo $get_arch
-case $get_arch in
-    "x86_64")
-        echo "x86_64"
-        ;;
-    "aarch64")
-        echo "arm64"
-        ;;
-    *)
-        echo "unknown arch"
-        ;;
-esac
+echo "------------------------------------------"
+echo "Architecture: $get_arch"
+echo "Python: $(which python3)"
+echo "------------------------------------------"
+
+PYBIND_PATH=$(python3 -c "import pybind11; print(pybind11.get_cmake_dir())" 2>/dev/null || echo "")
+
+if [ -z "$PYBIND_PATH" ]; then
+    echo "当前 Python 环境未安装 pybind11。"
+    exit 1
+fi
+
+if [ -d "build" ]; then
+    rm -rf build
+fi
 mkdir -p build
 cd build
-if [ $get_arch == "aarch64" ]; then
-    cmake -DIS_ARM=ON ..
+
+if [ "$get_arch" == "aarch64" ]; then
+    cmake -DIS_ARM=ON -Dpybind11_DIR="$PYBIND_PATH" ..
 else
-    cmake ..
+    cmake -Dpybind11_DIR="$PYBIND_PATH" ..
 fi
+
 make -j$(nproc)
+
 cd ..
 pip install -e .
